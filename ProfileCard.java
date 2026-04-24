15f8f4997834e67668eadcd2e25fdaad9c57cff1
PAULO <paulomayala245@gmail.com>
2026-04-25 02:29:25 +0300

    Finished assignment 0

--- ProfileCard.java
+++ ProfileCard.java
@@ -1,5 +1,5 @@
 public class ProfileCard {
-  public static void main(String[] args){
+  public static void main(String[] arguments){
     //student name
     String firstName = "Paulo";
     String lastName = " Bernado";
