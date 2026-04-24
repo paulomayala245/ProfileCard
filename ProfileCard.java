d6a5128cc873f604b3f2ea1e4f3e0268c380131f
paulomayala245 <paulomayala245@gmail.com>
2026-04-25 01:26:17 +0300

    Add files via upload

---
+++ ProfileCard.java
@@ -0,0 +1,31 @@
+public class ProfileCard {
+  public static void main(String[] args){
+    //student name
+    String firstName = "Paulo";
+    String lastName = " Bernado";
+    //student ID
+    String studentID = "2024-04-05974";
+    //year of study
+    String yearOfStudy = "2nd Year";
+    //programming background
+    String programmingBackground = "I have learnt  C programming language ";
+    //course goal
+    String courseGoal = "I want to become a skilled software engineer and good programmer";
+    //fun fact
+    String funFact = "I enjoy solving tech challenges in a short time";
+    //output (formatted profile card)
+    System.out.println("===========================================================================");
+    System.out.println("    CS 234 - JAVA PROFILE CARD");
+    System.out.println("===========================================================================");
+    System.out.println("Name            : " + firstName + " " + lastName); 
+		System.out.println("Student ID      : " + studentID);
+		System.out.println("Year of Study   : " + yearOfStudy);
+		System.out.println("--------------------------------------------------------------------------");
+		System.out.println("Background      : " + programmingBackground);
+		System.out.println("--------------------------------------------------------------------------");
+		System.out.println("Course goal     : " + courseGoal);
+	  System.out.println("------------------------------------------------------------------------");
+		System.out.println("Fun fact        : " + funFact);
+		System.out.println("==========================================================================");
+		}
+	}	
